8629386bd1920f05ed131af221b6ebdf83a7e212
DevOpsFairy <konstantinsnigirev92@gmail.com>
2024-02-25 18:25:31 +0000

    create acm.tf to get ssl certs

---
+++ cluster/acm.tf
@@ -0,0 +1,46 @@
+# configured aws provider with proper credentials
+provider "aws" {
+  region    = us-east-1
+  profile   = "student33_nov23_bootcamp_do_inter_fastapi"
+}
+
+# request public certificates from the amazon certificate manager.
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name               = "final-project.devops-fairy.cloudns.biz"
+  subject_alternative_names = ["*.final-project.devops-fairy.cloudns.biz"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# get details about a route 53 hosted zone
+data "aws_route53_zone" "route53_zone" {
+  name         = "final-project.devops-fairy.cloudns.biz"
+  private_zone = false
+}
+
+# create a record set in route 53 for domain validatation
+resource "aws_route53_record" "route53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.route53_zone.zone_id
+}
+
+# validate acm certificates
+resource "aws_acm_certificate_validation" "acm_certificate_validation" {
+  certificate_arn         = aws_acm_certificate.acm_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
+}
