8ad21e4848a1d514e0feca64cc476faa8293f940
Ubuntu <ubuntu@ip-172-31-36-252.eu-west-1.compute.internal>
2024-02-26 19:21:01 +0000

    Exposed metrics in app

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, Gauge, generate_latest, CONTENT_TYPE_LATEST
+from fastapi.responses import Response
 
 from app.db import database, User
 
 
 app = FastAPI(title="FastAPI, Docker, and Traefik")
 
+REQUEST_COUNT = Counter('request_count', 'App Request Count')
+IN_PROGRESS_REQUESTS = Gauge('in_progress_requests', 'In Progress Requests')
+
 
 @app.get("/")
 async def read_root():
+    REQUESTS.inc()
     return await User.objects.all()
 
+@app.get("/metrics")
+def metrics():
+    IN_PROGRESS_REQUESTS.inc()
+    data = generate_latest()
+    IN_PROGRESS_REQUESTS.dec()
+    return Response(content=data, media_type=CONTENT_TYPE_LATEST)
 
 @app.on_event("startup")
 async def startup():
