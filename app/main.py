3e0429b7a41f44b011dbcbe4c191595e75d3ea4c
StarOpsMaestro <153743273+StarOpsMaestro@users.noreply.github.com>
2024-02-26 20:10:35 +0100

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -16,19 +16,22 @@ async def read_root():
 async def startup():
     if not database.is_connected:
         await database.connect()
-    
+
     # Check if users table exists
     async with database:
-        users_table_exists = await database.fetch_val("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'users')")
-    
+        users_table_exists = await database.fetch_val(
+            "SELECT EXISTS (SELECT FROM information_schema.tables " +
+            "WHERE table_name = 'users')")
     # If users table doesn't exist, create it
     if not users_table_exists:
         async with database:
-            await database.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(128) UNIQUE NOT NULL, active BOOLEAN NOT NULL DEFAULT TRUE)")
-    
+            await database.execute(
+                "CREATE TABLE users " +
+                "(id SERIAL PRIMARY KEY, email VARCHAR(128) " +
+                "UNIQUE NOT NULL, active BOOLEAN NOT NULL DEFAULT TRUE)")
+
     # Now that the table exists, create dummy entries
     await User.objects.get_or_create(email="test@test.com")
-    await User.objects.get_or_create(email="test2@test.com")
 
 
 @app.on_event("shutdown")
